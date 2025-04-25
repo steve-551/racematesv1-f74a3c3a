81f3dc9df911d766784428022b6cb154ca8d4832
steve-551 <steve.horne555@gmail.com>
2025-04-25 19:56:02 +1000

    Create supabaseClient.ts

---
+++ src/lib/supabaseClient.ts
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = 'https://iflhdbjxonqtldorajuc.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlmbGhkYmp4b25xdGxkb3JhanVjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU1MDI1NDgsImV4cCI6MjA2MTA3ODU0OH0.dYpbRgXBgIAibYoLI3vGvY5UPAQ-Mj5FTM3M4x2CdDM';
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
